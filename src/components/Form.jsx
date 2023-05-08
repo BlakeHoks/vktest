9583e698903a2363d6943e931387bd65a1ff5a61
blakehoks <yasya.mir@ya.ru>
2023-05-08 19:03:40 +0300

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import Select from "react-select";
+import {MobileDateTimePicker} from "@mui/x-date-pickers";
+import {useState} from "react";
+import {towerOptions, floorOptions, roomOptions} from '../Options'
+import styles from './Form.module.css'
+import dayjs from "dayjs";
+
+const clearData = {
+    tower: "",
+    floor: "",
+    room: "",
+    dateTime: dayjs(null),
+    comment: ""
+}
+
+export const Form = () => {
+    const [data, setData] = useState(clearData)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(JSON.stringify(data))
+        setData(clearData)
+    }
+
+    const handleReset = (e) => {
+        e.preventDefault()
+        setData(clearData)
+    }
+
+    return(
+        <div className={styles.cont}>
+            <form className={styles.form}>
+                <Select value={data.tower ? towerOptions.find(c => c.value === data.tower) : null} className={styles.select} placeholder='Башня' options={towerOptions} onChange={opt => setData(prev => ({...prev, tower: opt.value}))}/>
+                <Select value={data.floor ? towerOptions.find(c => c.value === data.floor) : null} className={styles.select} placeholder='Этаж' options={floorOptions} onChange={opt => setData(prev => ({...prev, floor: opt.value}))}/>
+                <Select value={data.room ? towerOptions.find(c => c.value === data.room) : null} className={styles.select} placeholder='Переговорная' options={roomOptions} onChange={opt => setData(prev => ({...prev, room: opt.value}))}/>
+                <div className={styles.dateCont}>
+                    <MobileDateTimePicker value={data.dateTime} className={styles.date} disablePast onChange={opt => setData(prev => ({...prev, dateTime: opt}))}/>
+                </div>
+                <textarea value={data.comment} className={[styles.select, styles.input].join(' ')} placeholder='Комментарий' onChange={e => setData(prev => ({...prev, comment: e.target.value}))}/>
+                <div className={styles.btnCont}>
+                    <button className={[styles.btn, styles.accept].join(' ')} onClick={handleSubmit}>Отправить</button>
+                    <button className={[styles.btn, styles.reset].join(' ')} onClick={handleReset}>Очистить</button>
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import 'dayjs/locale/ru';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru"   >
+        <App />
+      </LocalizationProvider>
+  </React.StrictMode>,
+)
